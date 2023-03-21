8e91cb2a3f1a960ce9208b26eb229acca78752a1
Bhurva Sharma <me@bhurvasharma.com>
2023-03-21 21:20:49 +0530

    new code

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@ import { useState } from "react";
 import "./App.css";
 import FormInput from "./components/FormInput";
 
+
+
 const App = () => {
   const [values, setValues] = useState({
     username: "",
+    role:"",
     email: "",
     birthday: "",
     password: "",
     confirmPassword: "",
   });
+
+
 
   const inputs = [
     {
@@ -17,14 +22,26 @@ const App = () => {
       name: "username",
       type: "text",
       placeholder: "Username",
+      onChange:{InputEvent},
       errorMessage:
         "Username should be 3-16 characters and shouldn't include any special character!",
       label: "Username",
       pattern: "^[A-Za-z0-9]{3,16}$",
       required: true,
     },
+
     {
       id: 2,
+      type: FormDropdown,
+      placeholder: "Role",
+      label: "Role",
+      required: true,
+
+
+    },
+   
+    {
+      id: 3,
       name: "email",
       type: "email",
       placeholder: "Email",
@@ -33,14 +50,14 @@ const App = () => {
       required: true,
     },
     {
-      id: 3,
+      id: 4,
       name: "birthday",
       type: "date",
       placeholder: "Birthday",
       label: "Birthday",
     },
     {
-      id: 4,
+      id: 5,
       name: "password",
       type: "password",
       placeholder: "Password",
@@ -51,7 +68,7 @@ const App = () => {
       required: true,
     },
     {
-      id: 5,
+      id: 6,
       name: "confirmPassword",
       type: "password",
       placeholder: "Confirm Password",
@@ -60,11 +77,38 @@ const App = () => {
       pattern: values.password,
       required: true,
     },
+
+    
   ];
+
+
+  function FormDropdown(){
+    const [selectedOption, setSelectedOption] = useState('Option 1');
+
+    const handleOptionChange = (event) => {
+      setSelectedOption(event.target.value);
+    };
+
+    return (
+    <form>
+      <label htmlFor="dropdown"> Define Role: </label>
+      <select id="dropdown" value={selectedOption} onChange = {handleOptionChange} >
+        <option value="Student"> Student </option>
+        <option value="User"> User </option>
+        <option value="Admin"> Admin </option>
+      </select>
+    </form>
+    );
+  }
+
+
+
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
   };
+
 
   const onChange = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value });
@@ -83,9 +127,12 @@ const App = () => {
           />
         ))}
         <button>Submit</button>
+        <p>If you already have an account <a href='src\components\LoginForm.jsx'>Login Here</a></p>
       </form>
     </div>
   );
 };
+
+
 
 export default App;
